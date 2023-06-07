5648fad5351ce1c7e45f37dca44e1a11d11ae600
bariserdem <bariserdem@panteon.games>
2023-06-07 21:13:21 +0300

    Added Player Controller and Camera Follow
    Added Player Controller and Camera Follow

---
+++ Assets/Scripts/CamFollowPlayer.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollowPlayer : MonoBehaviour
+{
+    public Transform cameraTarget;
+    public float sSpeed = 10.0f;
+    public Vector3 dist;
+    public Transform lookTarget;
+
+    private void LateUpdate()
+    {
+        Vector3 dPos = cameraTarget.position + dist;
+        Vector3 sPos = Vector3.Lerp(transform.position, dPos, sSpeed * Time.deltaTime);
+        transform.position = sPos;
+        transform.LookAt(lookTarget.position);
+    }
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
