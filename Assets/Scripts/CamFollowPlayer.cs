903c20267382d2216010de080a492043a9af3476
bariserdem <bariserdem@panteon.games>
2023-06-12 20:30:22 +0300

    Added Swipe Check
    Added Swipe Check

--- Assets/Scripts/CamFollowPlayer.cs
+++ Assets/Scripts/CamFollowPlayer.cs
@@ -25,6 +25,6 @@ public class CamFollowPlayer : MonoBehaviour
     
     void Update()
     {
-        
+       
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float runningSpeed;
-    
+    public float xSpeed;
+    public float limitx;
     void Start()
     {
         
@@ -14,7 +15,26 @@ public class PlayerController : MonoBehaviour
     
     void Update()
     {
-        Vector3 newPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z + runningSpeed * Time.deltaTime);
+        SwipeCheck();
+    }
+
+    void SwipeCheck()
+    {
+        float newX = 0;
+        float touchXDelta = 0;
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+            //Debug.Log(Input.GetTouch(0).deltaPosition.x / Screen.width);
+            touchXDelta = Input.GetTouch(0).deltaPosition.x / Screen.width;
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            touchXDelta = Input.GetAxis("Mouse X");
+        }
+        newX = transform.position.x + xSpeed * touchXDelta * Time.deltaTime;
+        newX = Mathf.Clamp(newX, -limitx, limitx);
+
+        Vector3 newPosition = new Vector3(newX, transform.position.y, transform.position.z + runningSpeed * Time.deltaTime);
         transform.position = newPosition;
     }
 }
